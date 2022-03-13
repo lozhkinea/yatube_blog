78824ce38d095cba492eaffccebb97ce3eb4edcc
Эдуард Ложкин <lozhkinea@ya.ru>
2022-03-13 08:53:48 +0300

    change templates

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-
 
 handler403 = 'core.views.permission_denied'
 handler404 = 'core.views.page_not_found'
@@ -14,3 +14,8 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('about/', include('about.urls', namespace='about')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
